f98e74c7ec3a906918353c8c6ab79badd07ee71c
dbswjd7 <yj2171063@ewhain.net>
2025-12-30 18:00:25 +0900

    chore(#6): 머지중

--- src/main/java/com/example/Spot/global/exception/DuplicateUserException.java
+++
@@ -1,8 +0,0 @@
-package com.example.Spot.global.exception;
-
-public class DuplicateUserException extends RuntimeException {
-
-    public DuplicateUserException(String message) {
-        super(message);
-    }
-}
