193cd3743534816318a1cf2ddc9504bbb08a4b5f
dbswjd7 <yj2171063@ewhain.net>
2025-12-30 18:28:21 +0900

    feat(#2): JWT인증 토큰 구현

---
+++ src/main/java/com/example/Spot/infra/auth/security/CustomUserDetails.java
@@ -0,0 +1,81 @@
+package com.example.Spot.infra.auth.security;
+
+import com.example.Spot.user.domain.entity.UserEntity;
+import com.example.Spot.user.domain.entity.UserAuthEntity;
+import com.example.Spot.user.domain.repository.UserAuthRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final UserEntity userEntity;
+    private final UserAuthEntity userAuthEntity;
+
+    public CustomUserDetails(UserEntity userEntity,UserAuthEntity userAuthEntity) {
+        this.userEntity = userEntity;
+        this.userAuthEntity = userAuthEntity;
+    }
+
+    //JWT filter용
+    public CustomUserDetails(UserEntity userEntity) {
+        this.userEntity = userEntity;
+        this.userAuthEntity = null;
+    }
+
+    // role값 반환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+
+                return String.valueOf(userEntity.getRole());
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return (userAuthEntity == null) ? "" : userAuthEntity.getHashedPassword();
+    }
+
+
+    @Override
+    public String getUsername() {
+
+        return userEntity.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return true;
+    }
+}
