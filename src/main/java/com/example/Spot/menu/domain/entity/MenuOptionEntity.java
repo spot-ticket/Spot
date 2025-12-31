d2f72191d7a4966662d02b502b3c8c01694f2cde
first-lounge <zxcz9878@gmail.com>
2025-12-31 14:27:06 +0900

    style(#1): import문 수정

--- src/main/java/com/example/Spot/menu/domain/entity/MenuOptionEntity.java
+++ src/main/java/com/example/Spot/menu/domain/entity/MenuOptionEntity.java
@@ -6,11 +6,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
