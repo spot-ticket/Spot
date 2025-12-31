57b7c1624d79c443a35259d3c74de910e5787e1f
eqqmayo <eqqmayo@gmail.com>
2025-12-31 15:40:49 +0900

    feat(#1): Order, OrderItem Entity 에 BaseEntity 상속 적용

--- src/main/java/com/example/Spot/order/domain/entity/OrderItemEntity.java
+++ src/main/java/com/example/Spot/order/domain/entity/OrderItemEntity.java
@@ -1,39 +1,28 @@
 package com.example.Spot.order.domain.entity;
 
+import com.example.Spot.global.common.BaseEntity;
 import com.example.Spot.menu.domain.entity.MenuEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import com.example.Spot.menu.domain.entity.MenuEntity;
-import org.springframework.data.annotation.LastModifiedDate;
-import lombok.NoArgsConstructor;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
-import org.springframework.data.annotation.CreatedDate;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Getter
 @Table(name = "p_order_item")
-@EntityListeners(AuditingEntityListener.class)
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class OrderItemEntity {
+public class OrderItemEntity extends BaseEntity {
 
     @Id
+    @GeneratedValue
     @UuidGenerator
+    @Column(columnDefinition = "UUID")
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
+    @JoinColumn(name = "order_id", nullable = false)
     private OrderEntity order;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -49,16 +38,12 @@ public class OrderItemEntity {
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
 
-    @Builder.Default
-    @Column(name = "is_deleted", nullable = false)
-    private Boolean isDeleted = false;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+    @Builder
+    public OrderItemEntity(OrderEntity order, MenuEntity menu, 
+                          BigDecimal menuPrice, Integer quantity) {
+        this.order = order;
+        this.menu = menu;
+        this.menuPrice = menuPrice;
+        this.quantity = quantity;
+    }
 }
-
