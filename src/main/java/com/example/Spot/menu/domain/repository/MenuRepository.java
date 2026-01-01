0b426ffcc4610d426670adf2ea3596fe0c5227a8
first-lounge <zxcz9878@gmail.com>
2026-01-01 23:03:23 +0900

    feat(#3): 메뉴 repository 생성

--- src/main/java/com/example/Spot/menu/domain/repository/MenuRepository.java
+++ src/main/java/com/example/Spot/menu/domain/repository/MenuRepository.java
@@ -1,4 +1,21 @@
 package com.example.Spot.menu.domain.repository;
 
-public interface MenuRepository {
+import com.example.Spot.menu.domain.entity.MenuEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MenuRepository extends JpaRepository<MenuEntity, UUID>{
+
+    // [손님용] 가게 메뉴 조회 (삭제 X, 숨김 X)
+    List<MenuEntity> findAllByStoreIdAndIsDeletedFalseAndIsHiddenFalse(UUID storeId);
+
+    // [손님용] 메뉴 ID로 조회한 경우 (삭제 O)
+    Optional<MenuEntity> findAllByIdAndIsDeletedFalse(UUID menuId);
+
+    // [가게 주인용] 가게 메뉴 조회 (삭제 X, 숨김 O)
+    List<MenuEntity> findAllByStoreIdAndIsDeletedFalse(UUID storeId);
+
 }
