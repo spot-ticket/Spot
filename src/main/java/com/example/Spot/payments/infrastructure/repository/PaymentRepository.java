642b4b70e81902658bdf827ac894e9f3fb68c576
yoonchulchung <yoonchul005@gmail.com>
2026-01-02 15:45:34 +0900

    fix(#1): 코드 import 순서 수정

--- src/main/java/com/example/Spot/payments/infrastructure/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Spot.payments.infrastructure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Spot.payments.domain.entity.PaymentEntity;
-
-public interface PaymentRepository extends JpaRepository<PaymentEntity, Integer> {
-
-}
