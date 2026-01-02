b9140095357fb6c2d815438c5599e6d5ce347cca
yoonchulchung <yoonchul005@gmail.com>
2026-01-02 13:14:39 +0900

    fix(#1): fix-error

--- src/test/java/com/example/Spot/menu/domain/entity/OriginEntityTest.java
+++ src/test/java/com/example/Spot/menu/domain/entity/OriginEntityTest.java
@@ -1,5 +1,11 @@
 package com.example.Spot.menu.domain.entity;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class OriginEntityTest {
 
     @Test
