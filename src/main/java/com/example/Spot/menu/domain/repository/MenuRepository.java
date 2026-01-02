fd72bace29a03d72821caa57a2a91f9c6e44f917
first-lounge <zxcz9878@gmail.com>
2026-01-02 14:56:54 +0900

    fix(#3): 손님용 조회는 jpql로 수정

--- src/main/java/com/example/Spot/menu/domain/repository/MenuRepository.java
+++ src/main/java/com/example/Spot/menu/domain/repository/MenuRepository.java
@@ -2,6 +2,8 @@ package com.example.Spot.menu.domain.repository;
 
 import com.example.Spot.menu.domain.entity.MenuEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,10 +12,12 @@ import java.util.UUID;
 public interface MenuRepository extends JpaRepository<MenuEntity, UUID>{
 
     // [손님용] 가게 메뉴 조회 (삭제 X, 숨김 X)
-    List<MenuEntity> findAllByStoreIdAndIsDeletedFalseAndIsHiddenFalse(UUID storeId);
+    @Query("select m from MenuEntity m where m.store.id = :storeId AND m.isDeleted = false AND m.isHidden = false")
+    List<MenuEntity> findAllActiveMenus(@Param("storeId") UUID storeId);
 
-    // [손님용] 메뉴 ID로 조회한 경우 (삭제 X)
-    Optional<MenuEntity> findByIdAndIsDeletedFalse(UUID menuId);
+    // [손님용] 메뉴 ID로 조회한 경우 (삭제 X, 숨김 X)
+    @Query("select m from MenuEntity m where m.id = :menuId AND m.isDeleted = false AND m.isHidden = false")
+    Optional<MenuEntity> findActiveMenuById(@Param("menuId") UUID menuId);
 
     // [가게 주인용] 가게 메뉴 조회 (삭제 X, 숨김 O)
     List<MenuEntity> findAllByStoreIdAndIsDeletedFalse(UUID storeId);
