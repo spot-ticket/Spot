af5e4e23c67190dd216ae292ef85cb3d33c9a2a0
dbswjd7 <yj2171063@ewhain.net>
2026-01-02 15:03:37 +0900

    style(#1): pr checking

--- src/main/java/com/example/Spot/user/domain/entity/RefreshTokenEntity.java
+++ src/main/java/com/example/Spot/user/domain/entity/RefreshTokenEntity.java
@@ -1,7 +1,16 @@
 package com.example.Spot.user.domain.entity;
 
 import com.example.Spot.global.common.UpdateBaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
