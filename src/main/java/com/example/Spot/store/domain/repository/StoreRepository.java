7f53fd151912e27555760a18c79243e78f3080d0
dbswjd7 <yj2171063@ewhain.net>
2026-01-04 17:13:13 +0900

    fix(#68): pr check

--- src/main/java/com/example/Spot/store/domain/repository/StoreRepository.java
+++ src/main/java/com/example/Spot/store/domain/repository/StoreRepository.java
@@ -42,4 +42,12 @@ public interface StoreRepository extends JpaRepository<StoreEntity, UUID> {
             "AND u.role = ('OWNER', 'CHEF') " +
             "AND s.isDeleted = false")
     List<StoreEntity> findAllByOwnerId(@Param("userId") Long userId);
+
+    // category-repo
+    // 삭제되지 않은 가게 전체 조회
+    List<StoreEntity> findByIsDeletedFalse();
+
+    // ID로 삭제되지 않은 가게 조회
+    Optional<StoreEntity> findByIdAndIsDeletedFalse(UUID id);
+
 }
