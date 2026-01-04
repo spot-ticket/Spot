87c40b4aaad40e3737a82811fb2c2e9d8fae289c
first-lounge <137966925+first-lounge@users.noreply.github.com>
2026-01-05 02:36:56 +0900

    feat(#4): 메뉴와 메뉴 옵션의 entity, repository 테스트 코드 수정 및 DTO PR check 원인 수정 (#76)
    메뉴와 메뉴 옵션의 entity, repository 테스트 코드 수정 및 DTO PR check 원인 수정

--- src/main/java/com/example/Spot/menu/presentation/dto/response/DeleteMenuResponseDto.java
+++ src/main/java/com/example/Spot/menu/presentation/dto/response/DeleteMenuResponseDto.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import com.example.Spot.menu.domain.entity.MenuEntity;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
