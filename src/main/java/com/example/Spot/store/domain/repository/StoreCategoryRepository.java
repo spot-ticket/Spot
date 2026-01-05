d46cf01c649074bfb468ab450cf218a75a4902fb
yunjeong <99129298+dbswjd7@users.noreply.github.com>
2026-01-05 15:11:23 +0900

    Revert "Feat(#4) category service, controller 생성" (#89)
    Reverts spot-ticket/Spot#75

--- src/main/java/com/example/Spot/store/domain/repository/StoreCategoryRepository.java
+++ src/main/java/com/example/Spot/store/domain/repository/StoreCategoryRepository.java
@@ -14,25 +14,20 @@ import com.example.Spot.store.domain.entity.StoreEntity;
 
 public interface StoreCategoryRepository extends JpaRepository<StoreCategoryEntity, UUID> {
 
-    // Category - 카테고리 별 매장 조회
     @Query("""
-        select scm
-        from StoreCategoryEntity scm
-        join fetch scm.store s  
-        where scm.category.id  = :categoryId
+        select sc
+        from StoreCategoryEntity sc
+        join fetch sc.store s
+        where sc.isDeleted = false
           and s.isDeleted = false
-          and s.isDeleted = false
+          and sc.category.id = :categoryId
     """)
     List<StoreCategoryEntity> findAllActiveByCategoryIdWithStore(@Param("categoryId") UUID categoryId);
 
-
-    // Store
     List<StoreCategoryEntity> findByCategoryAndIsDeletedFalse(CategoryEntity category);
 
     List<StoreCategoryEntity> findByStoreAndIsDeletedFalse(StoreEntity store);
 
-
-    // Test
     Optional<StoreCategoryEntity> findByStoreAndCategoryAndIsDeletedFalse(
             StoreEntity store,
             CategoryEntity category
