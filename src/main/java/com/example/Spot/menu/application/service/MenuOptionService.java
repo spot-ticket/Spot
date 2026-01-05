3b097a6b96a1bb7b74adf76ffef32b4bb45f8cfa
first-lounge <zxcz9878@gmail.com>
2026-01-06 00:50:19 +0900

    feat(#4): 메뉴 옵션 Service 추가

--- src/main/java/com/example/Spot/menu/application/service/MenuOptionService.java
+++ src/main/java/com/example/Spot/menu/application/service/MenuOptionService.java
@@ -1,4 +1,15 @@
 package com.example.Spot.menu.application.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import com.example.Spot.menu.presentation.dto.request.*;
+import com.example.Spot.menu.presentation.dto.response.*;
+import com.example.Spot.user.domain.Role;
+
 public interface MenuOptionService {
+    List<MenuOptionResponseDto> getOptions(UUID menuId, Role userRole);
+    CreateMenuOptionResponseDto createMenuOption(UUID menuId, CreateMenuOptionRequestDto request);
+    UpdateMenuOptionResponseDto updateMenuOption(UUID optionId, UpdateMenuOptionRequestDto request);
+    void deleteMenuOption(UUID optionId);
 }
