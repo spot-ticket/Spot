301b4c9b19881796de71d886e1fd12af57ea7a46
Yoonchul Chung <84674889+Yoonchulchung@users.noreply.github.com>
2026-01-05 15:28:40 +0900

    Revert "feat(#66): payment controller 추가 (#86)"
    This reverts commit 020039c605b2b107d8e7b939f0f1e946f4df9731.

--- src/main/java/com/example/Spot/store/domain/repository/StoreRepository.java
+++ src/main/java/com/example/Spot/store/domain/repository/StoreRepository.java
@@ -15,7 +15,7 @@ import com.example.Spot.store.domain.entity.StoreEntity;
 public interface StoreRepository extends JpaRepository<StoreEntity, UUID> {
 
     // 기본 조회
-    @Query("SELECT s FROM StoreEntity s " +
+    @Query("SELECT s FROM StoreEntity s " + 
             "LEFT JOIN FETCH s.storeCategoryMaps sc " +
             "LEFT JOIN FETCH sc.category " +
             "WHERE (:isAdmin = true OR s.isDeleted = false)")
@@ -25,7 +25,7 @@ public interface StoreRepository extends JpaRepository<StoreEntity, UUID> {
     @Query("SELECT s FROM StoreEntity s " +
             "LEFT JOIN FETCH s.storeCategoryMaps sc " +
             "LEFT JOIN FETCH sc.category " +
-            "LEFT JOIN FETCH s.users su " +
+            "LEFT JOIN FETCH s.users su " + 
             "LEFT JOIN FETCH su.user u " +
             "WHERE s.id = :id " +
             "AND (:isAdmin = true OR s.isDeleted = false)")
@@ -33,7 +33,7 @@ public interface StoreRepository extends JpaRepository<StoreEntity, UUID> {
 
     // 검색 기능
     List<StoreEntity> findByNameContainingAndIsDeletedFalse(String name);
-
+    
     // 특정 유저가 담당하는 매장 조회 (중간 테이블 Join)
     @Query("SELECT s FROM StoreEntity s " +
             "JOIN s.users su " +
@@ -42,7 +42,6 @@ public interface StoreRepository extends JpaRepository<StoreEntity, UUID> {
             "AND u.role IN ('OWNER', 'CHEF') " +
             "AND s.isDeleted = false")
     List<StoreEntity> findAllByOwnerId(@Param("userId") Integer userId);
-
 
     // category-repo
     // 삭제되지 않은 가게 전체 조회
