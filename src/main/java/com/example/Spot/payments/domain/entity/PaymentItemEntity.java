a72332a749373651178f3dfa98a90dee7fab79c0
Yoonchul Chung <84674889+Yoonchulchung@users.noreply.github.com>
2026-01-05 13:18:58 +0900

    feat(#3): payment repository (#83)
    Toss Billing API 추가.
    Payment Repository 추가.
    Payment Entity에서 Payment Item Entity로 상태 변경.

--- src/main/java/com/example/Spot/payments/domain/entity/PaymentItemEntity.java
+++ src/main/java/com/example/Spot/payments/domain/entity/PaymentItemEntity.java
@@ -11,12 +11,13 @@ import com.example.Spot.order.domain.entity.OrderEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -40,12 +41,16 @@ public class PaymentItemEntity extends BaseEntity {
     @JoinColumn(name = "payment_id")
     private PaymentEntity payment;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private OrderEntity order;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "payment_status")
+    private PaymentStatus paymentStatus;
+
     @Builder
-    public PaymentItemEntity(PaymentEntity payment, OrderEntity order) {
+    public PaymentItemEntity(PaymentEntity payment, OrderEntity order, PaymentStatus status) {
 
         if (payment == null) {
             throw new IllegalArgumentException("사전에 등록된 결제가 있어야 합니다.");
@@ -56,5 +61,21 @@ public class PaymentItemEntity extends BaseEntity {
 
         this.payment = payment;
         this.order = order;
+        this.paymentStatus = status;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return this.paymentStatus;
+    }
+    
+    public enum PaymentStatus {
+        READY,               // 결제 준비
+        IN_PROGRESS,         // 소유자가 맞는지 인증을 마친 상태
+        WAITING_FOR_DEPOSIT, // 가상계좌 결제 흐름에만 있는 상태
+        DONE,                // 인증된 결제수단으로 요청한 결제가 승인된 상태
+        CANCELLED,           // 결제 취소
+        PARTIAL_CANCELD,     // 승인된 결제가 부분 취소된 상태
+        ABORTED,             // 결제 승인이 실패한 상태
+        EXPIRED              // 결제 유효 시간 30분이 지나 거래가 취소 상태. IN_PROGRESS 상태에서 결제 승인 API를 호출하지 않으면 EXPIRED가 됨.
     }
 }
