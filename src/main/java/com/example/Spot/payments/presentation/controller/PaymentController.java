b2dba793a25bc56b53c2d970a5603a23141229f4
yoonchulchung <yoonchul005@gmail.com>
2026-01-05 16:27:14 +0900

    feat(#68): payment controller 추가

--- src/main/java/com/example/Spot/payments/presentation/controller/PaymentController.java
+++ src/main/java/com/example/Spot/payments/presentation/controller/PaymentController.java
@@ -1,6 +1,11 @@
 package com.example.Spot.payments.presentation.controller;
 
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +15,7 @@ import com.example.Spot.payments.application.service.PaymentService;
 import com.example.Spot.payments.presentation.dto.request.PaymentRequestDto;
 import com.example.Spot.payments.presentation.dto.response.PaymentResponseDto;
 
-
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,10 +26,46 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/confirm")
-    public ApiResponse<PaymentResponseDto.Confirm> paymentBillingConfirm(
-        PaymentRequestDto.Confirm request
+    public ApiResponse<PaymentResponseDto.Confirm> confirmPayment(
+        @Valid @RequestBody PaymentRequestDto.Confirm request
     ) {
-        PaymentResponseDto.Confirm response = paymentService.paymentBillingConfirm(request);
+        PaymentResponseDto.Confirm response = paymentService.confirmPaymentBilling(request);
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, response);
+    }
+
+    @PostMapping("/cancel")
+    public ApiResponse<PaymentResponseDto.Cancel> cancelPayment(
+        @Valid @RequestBody PaymentRequestDto.Cancel request
+    ) {
+        PaymentResponseDto.Cancel response = paymentService.cancelPayment(request);
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, response);
+    }
+
+    @GetMapping
+    public ApiResponse<PaymentResponseDto.PaymentList> getAllPayment() {
+        PaymentResponseDto.PaymentList response = paymentService.getAllPayment();
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, response);
+    }
+
+    @GetMapping("/{paymentId}")
+    public ApiResponse<PaymentResponseDto.PaymentDetail> getDetailPayment(
+        @PathVariable UUID paymentId
+    ) {
+        PaymentResponseDto.PaymentDetail response = paymentService.getDetailPayment(paymentId);
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, response);
+    }
+
+    @GetMapping("/cancel")
+    public ApiResponse<PaymentResponseDto.CancelList> getAllPaymentCancel() {
+        PaymentResponseDto.CancelList response = paymentService.getAllPaymentCancel();
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, response);
+    }
+
+    @GetMapping("/{paymentId}/cancel")
+    public ApiResponse<PaymentResponseDto.CancelList> getDetailPaymentCancel(
+        @PathVariable UUID paymentId
+    ) {
+        PaymentResponseDto.CancelList response = paymentService.getDetailPaymentCancel(paymentId);
         return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, response);
     }
 
