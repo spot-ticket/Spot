fca696e4e25527a0f060895bac37839394bd10d5
yunjeong <99129298+dbswjd7@users.noreply.github.com>
2026-01-05 14:53:01 +0900

    Feat(#3) Category repository, dto 구현 (#73)
    Category관련 repository, dto를 생성했습니다.
    StoreCategoryRespository, CategoryRepository,
    CategoryRequestDTO, CategoryResponseDTO 구현
    
    이외 수정 파일들은 머지과정에서 수정한 파일들입니다.

--- src/main/java/com/example/Spot/store/domain/repository/CategoryRepository.java
+++ src/main/java/com/example/Spot/store/domain/repository/CategoryRepository.java
@@ -12,10 +12,17 @@ import com.example.Spot.store.domain.entity.CategoryEntity;
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, UUID> {
 
-    Optional<CategoryEntity> findByName(String name);
 
-    // soft delete 컬럼(isDeleted)이 UpdateBaseEntity에 있다고 가정
-    List<CategoryEntity> findByIsDeletedFalse();
+    // main
+    List<CategoryEntity> findAllByIsDeletedFalse();
 
     Optional<CategoryEntity> findByIdAndIsDeletedFalse(UUID id);
+
+    boolean existsByNameAndIsDeletedFalse(String name);
+
+
+    // test
+    Optional<CategoryEntity> findByName(String name);
+    // soft delete 컬럼(isDeleted)이 UpdateBaseEntity에 있다고 가정
+    List<CategoryEntity> findByIsDeletedFalse();
 }
