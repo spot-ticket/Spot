985a7fed8875a44a8f7c843213334699c2611a42
first-lounge <zxcz9878@gmail.com>
2026-01-06 20:06:36 +0900

    fix(#68): AllArgs, Builder 어노테이션 추가

--- src/main/java/com/example/Spot/menu/presentation/dto/request/UpdateMenuHiddenRequestDto.java
+++ src/main/java/com/example/Spot/menu/presentation/dto/request/UpdateMenuHiddenRequestDto.java
@@ -2,11 +2,15 @@ package com.example.Spot.menu.presentation.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UpdateMenuHiddenRequestDto {
 
     @JsonProperty("is_hidden")
