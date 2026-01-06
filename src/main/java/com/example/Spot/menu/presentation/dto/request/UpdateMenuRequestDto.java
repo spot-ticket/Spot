60cc8d5ff17dae5565a369cb6475b46c41edfb0f
first-lounge <137966925+first-lounge@users.noreply.github.com>
2026-01-06 22:52:58 +0900

    feat(#68): menu controller (#112)
    1. request dto에 Allargs와 Builder 어노테이션 추가
    2. response dto의 MenuPublicResponseDto에서 isHidden 삭제
    3. Menu Service에 유저의 권한과 소속 가게 검증 로직 추가
    4. 메뉴 Controller 및 테스트 코드 작성
    5. 메뉴 Service와 Repository 테스트 코드 수정

--- src/main/java/com/example/Spot/menu/presentation/dto/request/UpdateMenuRequestDto.java
+++ src/main/java/com/example/Spot/menu/presentation/dto/request/UpdateMenuRequestDto.java
@@ -2,11 +2,15 @@ package com.example.Spot.menu.presentation.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor // Builder 사용을 위해 필요
+@Builder            // 테스트 및 코드 작성 시 편리함 제공
 public class UpdateMenuRequestDto {
     private String name;
     private String category;
