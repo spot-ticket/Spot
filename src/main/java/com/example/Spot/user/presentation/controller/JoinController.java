0a8041faecfee2c331059643747de6db7e016863
dbswjd7 <yj2171063@ewhain.net>
2026-01-06 19:33:12 +0900

    fix(#68): pr check

--- src/main/java/com/example/Spot/user/presentation/controller/JoinController.java
+++ src/main/java/com/example/Spot/user/presentation/controller/JoinController.java
@@ -1,31 +1,24 @@
 package com.example.Spot.user.presentation.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Spot.user.application.service.JoinService;
 import com.example.Spot.user.presentation.dto.request.JoinDTO;
 
-@Controller
-@ResponseBody
-public class JoinController {
 
+@RestController
+public class JoinController {
 
     private final JoinService joinService;
 
     public JoinController(JoinService joinService) {
-
         this.joinService = joinService;
     }
 
     @PostMapping("/api/join")
-    public String joinProcess(@RequestBody JoinDTO joinDTO) {
-
-        System.out.println(joinDTO.getUsername());
+    public void joinProcess(@RequestBody JoinDTO joinDTO) {
         joinService.joinProcess(joinDTO);
-
-        return "ok";
     }
 }
