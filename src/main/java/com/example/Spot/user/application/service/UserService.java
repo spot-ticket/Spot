0933de80afa98c7ac2b0b97817dda765ff76970e
yunjeong <99129298+dbswjd7@users.noreply.github.com>
2026-01-06 11:32:50 +0900

    feat(#4): jwttoken에 userid 추가 및 refresh token flow를 jwt로 (#104)
    - 기존 JWT token의 구성요소를 (username, role) -> (userid, role)로 변경했습니다. 
    또한 Refresh token도 JWT 로직에 포함시키도록 로직을 변경했습니다. (entity, repository 삭제)
    관련해서 크게 변경하실 로직은 없으나 controller단에서 @PreAuthorize를 통해 username을 검증하고 계셨다면
    userid로 바꾸시면 됩니다.
    `#userId==authentication.principal`
    
    - 회원 조회, 수정, 삭제 endpoint url을 변경하였고, API 명세서에 반영하였습니다.
        - username -> userId (조회, 수정)
        - username -> me (삭제)

--- src/main/java/com/example/Spot/user/application/service/UserService.java
+++ src/main/java/com/example/Spot/user/application/service/UserService.java
@@ -1,6 +1,5 @@
 package com.example.Spot.user.application.service;
 
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,15 +17,15 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserResponseDTO getByUsername(String username) {
-        UserEntity user = userRepository.findByUsername(username)
+    public UserResponseDTO getByUserId(Integer userid) {
+        UserEntity user = userRepository.findById(userid)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
         return toResponse(user);
     }
 
     @Transactional
-    public UserResponseDTO updateByUsername(String username, UserUpdateRequestDTO req) {
-        UserEntity user = userRepository.findByUsername(username)
+    public UserResponseDTO updateById(Integer userid, UserUpdateRequestDTO req) {
+        UserEntity user = userRepository.findById(userid)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
         if (req.nickname() != null) {
@@ -42,17 +41,15 @@ public class UserService {
         return toResponse(user);
     }
 
-    @Transactional
-    public void deleteByUsername(String targetUsername, String loginUsername) {
-        if (!targetUsername.equals(loginUsername)) {
-            throw new AccessDeniedException("사용자 본인만 삭제할 수 있습니다.");
-        }
 
-        UserEntity user = userRepository.findByUsername(targetUsername)
+    @Transactional
+    public void deleteMe(Integer loginUserId) {
+        UserEntity user = userRepository.findById(loginUserId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
         user.softDelete();
     }
+
 
     private UserResponseDTO toResponse(UserEntity user) {
         return new UserResponseDTO(
