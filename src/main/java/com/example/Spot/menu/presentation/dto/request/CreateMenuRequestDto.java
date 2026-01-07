2e8befaa089ae81917fbf087f876984d69db4532
first-lounge <zxcz9878@gmail.com>
2026-01-07 09:32:05 +0900

    style(#68): AllArgsConstructor, Builder import문 삭제

--- src/main/java/com/example/Spot/menu/presentation/dto/request/CreateMenuRequestDto.java
+++ src/main/java/com/example/Spot/menu/presentation/dto/request/CreateMenuRequestDto.java
@@ -9,11 +9,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 @Getter
 @NoArgsConstructor
