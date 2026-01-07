59952ab22aee7d56ca0708afe63d638a9213d90d
yunjeong <99129298+dbswjd7@users.noreply.github.com>
2026-01-07 18:55:28 +0900

    Feat/121 globalentity auditor (#145)
    created_by 를 위한 AuditorAwareImpl, JpaAuditConfig 생성

--- src/main/java/com/example/Spot/global/common/AuditorAwareImpl.java
+++ src/main/java/com/example/Spot/global/common/AuditorAwareImpl.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import com.example.Spot.infra.auth.security.CustomUserDetails;
 
 @Component
 public class AuditorAwareImpl implements AuditorAware<Integer> {
@@ -17,6 +16,8 @@ public class AuditorAwareImpl implements AuditorAware<Integer> {
     public Optional<Integer> getCurrentAuditor() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
+
+        // 회원가입: null이므로 0 (SYSTEM)
         if (auth == null
                 || !auth.isAuthenticated()
                 || auth instanceof AnonymousAuthenticationToken) {
@@ -24,10 +25,19 @@ public class AuditorAwareImpl implements AuditorAware<Integer> {
         }
 
         Object principal = auth.getPrincipal();
-        if (principal instanceof CustomUserDetails cud) {
-            return Optional.of(cud.getUserId());
+
+
+        // 1) principal을 Integer로
+        if (principal instanceof Integer id) {
+            return Optional.of(id);
         }
 
+        // if... CustomUserDetails
+//        if (principal instanceof CustomUserDetails cud) {
+//            return Optional.ofNullable(cud.getUserId()).or(() -> Optional.of(0));
+//        }
+
+        // 2) 그 외는 SYSTEM
         return Optional.of(0);
     }
 }
