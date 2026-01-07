1e795dbe5d39aa576a24ca127a74e88446527725
first-lounge <137966925+first-lounge@users.noreply.github.com>
2026-01-07 09:43:54 +0900

    feat(#68): menu option controller (#116)
    1. 메뉴 옵션 Controller 작성
    2. 메뉴 Controller 수정
    3. 메뉴 옵션 Service 수정
    4. 메뉴 Service, Controller 테스트 코드 수정

---
+++ src/main/java/com/example/Spot/menu/presentation/controller/MenuOptionController.java
@@ -0,0 +1,92 @@
+package com.example.Spot.menu.presentation.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Spot.global.presentation.ApiResponse;
+import com.example.Spot.global.presentation.code.GeneralSuccessCode;
+import com.example.Spot.infra.auth.security.CustomUserDetails;
+import com.example.Spot.menu.application.service.MenuOptionService;
+import com.example.Spot.menu.presentation.dto.request.CreateMenuOptionRequestDto;
+import com.example.Spot.menu.presentation.dto.request.UpdateMenuOptionRequestDto;
+import com.example.Spot.menu.presentation.dto.response.CreateMenuOptionResponseDto;
+import com.example.Spot.menu.presentation.dto.response.MenuOptionResponseDto;
+import com.example.Spot.menu.presentation.dto.response.UpdateMenuOptionResponseDto;
+import com.example.Spot.user.domain.Role;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/stores/{storeId}/menus/{menuId}/options")
+@RequiredArgsConstructor
+public class MenuOptionController {
+
+    private final MenuOptionService menuOptionService;
+
+    // 메뉴 옵션 조회
+    @PreAuthorize("hasAnyRole('MASTER', 'MANAGER', 'OWNER')")
+    @GetMapping
+    public ApiResponse<List<MenuOptionResponseDto>> getOptions(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable UUID storeId,
+            @PathVariable UUID menuId
+    ) {
+        Role userRole = user.getUserEntity().getRole();
+
+        List<MenuOptionResponseDto> data = menuOptionService.getOptions(userRole, storeId, menuId);
+
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, data);
+    }
+
+    // 메뉴 옵션 생성
+    @PreAuthorize("hasAnyRole('MASTER', 'MANAGER', 'OWNER')")
+    @PostMapping
+    public ApiResponse<CreateMenuOptionResponseDto> createMenuOption(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable UUID storeId,
+            @PathVariable UUID menuId,
+            @RequestBody CreateMenuOptionRequestDto request
+    ) {
+        CreateMenuOptionResponseDto data = menuOptionService.createMenuOption(user.getUserEntity(), storeId, menuId, request);
+
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, data);
+    }
+
+    // 메뉴 옵션 변경
+    @PreAuthorize("hasAnyRole('MASTER', 'MANAGER', 'OWNER')")
+    @PatchMapping("/{optionId}")
+    public ApiResponse<UpdateMenuOptionResponseDto> updateMenuOption(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable UUID storeId,
+            @PathVariable UUID optionId,
+            @RequestBody UpdateMenuOptionRequestDto request
+    ) {
+        UpdateMenuOptionResponseDto data = menuOptionService.updateMenuOption(user.getUserEntity(), storeId, optionId, request);
+
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, data);
+    }
+
+    // 메뉴 옵션 삭제
+    @PreAuthorize("hasAnyRole('MASTER', 'MANAGER', 'OWNER')")
+    @DeleteMapping("/{optionId}")
+    public ApiResponse<String> deleteMenuOption(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable UUID storeId,
+            @PathVariable UUID optionId
+    ) {
+        menuOptionService.deleteMenuOption(user.getUserEntity(), storeId, optionId);
+
+        return ApiResponse.onSuccess(GeneralSuccessCode.GOOD_REQUEST, "해당 옵션이 삭제되었습니다.");
+    }
+}
