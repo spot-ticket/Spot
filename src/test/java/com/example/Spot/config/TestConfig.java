1da96c2e9f3526b05281738c0b825224fbb3e74a
dbswjd7 <yj2171063@ewhain.net>
2026-01-08 18:02:11 +0900

    test(#117): user test 확인

--- src/test/java/com/example/Spot/config/TestConfig.java
+++ src/test/java/com/example/Spot/config/TestConfig.java
@@ -2,10 +2,8 @@ package com.example.Spot.config;
 
 import java.util.Optional;
 
-import com.example.Spot.user.infrastructure.repository.FakeEmailSender;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,15 +24,4 @@ public class TestConfig {
         return () -> Optional.of(1);
     }
 
-    @Bean
-    @Primary
-    public FakeEmailSender fakeEmailSender() {
-        return new FakeEmailSender();
-    }
-
-    @Bean
-    @Primary
-    public EmailSender emailSender(FakeEmailSender fake) {
-        return fake; // EmailSender 주입 자리에 fake로 대체
-    }
 }
