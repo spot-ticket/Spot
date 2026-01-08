fb40afda04965af597f6ebf8b132100bfbc94ae8
yoonchulchung <yoonchul005@gmail.com>
2026-01-08 22:56:30 +0900

    fix(#162): user id 조회 방법 변경

--- src/main/java/com/example/Spot/payments/presentation/controller/PaymentController.java
+++ src/main/java/com/example/Spot/payments/presentation/controller/PaymentController.java
@@ -13,13 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Spot.global.presentation.ApiResponse;
 import com.example.Spot.global.presentation.code.GeneralSuccessCode;
-import com.example.Spot.infra.auth.security.CustomUserDetails;
-import com.example.Spot.user.domain.entity.UserEntity;
-import com.example.Spot.user.domain.repository.UserRepository;
 import com.example.Spot.payments.application.service.PaymentService;
 import com.example.Spot.payments.presentation.dto.request.PaymentRequestDto;
 import com.example.Spot.payments.presentation.dto.response.PaymentResponseDto;
 import com.example.Spot.user.domain.Role;
+import com.example.Spot.user.domain.entity.UserEntity;
+import com.example.Spot.user.domain.repository.UserRepository;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
