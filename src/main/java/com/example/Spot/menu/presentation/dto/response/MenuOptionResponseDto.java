439625878316018a993a38db5f273599555b698e
Yoonchul Chung <84674889+Yoonchulchung@users.noreply.github.com>
2026-01-10 14:58:12 +0900

    feat(#192): payment 로직 수정 (#194)
    기존에 Payment 결제가 안되는 문제를 해결했습니다. FE 코드 개발을 고려하지 않고 BillingKey를 하드 코딩으로
    저장하는 방식에서 FE에서 등록되면 BE로 보내서 저장하는 방식으로 변경했습니다.
    
    일부 Menu와 Store, Order에서 FE에서 조회가 안되는 문제를 수정했습니다.

--- src/main/java/com/example/Spot/menu/presentation/dto/response/MenuOptionResponseDto.java
+++ src/main/java/com/example/Spot/menu/presentation/dto/response/MenuOptionResponseDto.java
@@ -3,7 +3,6 @@ package com.example.Spot.menu.presentation.dto.response;
 import java.util.UUID;
 
 import com.example.Spot.menu.domain.entity.MenuOptionEntity;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,28 +11,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MenuOptionResponseDto {
 
-    @JsonProperty("option_id")
     private UUID id;
 
-    @JsonProperty("menu_id")
     private UUID menuId;
 
-    private String name;
-    private String detail;
-    private Integer price;
+    private String optionName;
 
-    @JsonProperty("is_available")
+    private String detail;
+
+    private Integer optionPrice;
+
     private Boolean isAvailable;
 
-    @JsonProperty("is_deleted")
     private Boolean isDeleted;
 
     public MenuOptionResponseDto(MenuOptionEntity menuOption) {
         this.id = menuOption.getId();
         this.menuId = menuOption.getMenu().getId();
-        this.name = menuOption.getName();
+        this.optionName = menuOption.getName();
         this.detail = menuOption.getDetail();
-        this.price = menuOption.getPrice();
+        this.optionPrice = menuOption.getPrice();
         this.isAvailable = menuOption.getIsAvailable();
         this.isDeleted = menuOption.getIsDeleted();
     }
