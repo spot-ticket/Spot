e273650f5ed4eb489b129626a739daa074935b03
yoonchulchung <yoonchul005@gmail.com>
2026-01-11 00:27:33 +0900

    fix(#199): 사용하지 않는 import 제거

--- src/main/java/com/example/Spot/menu/presentation/dto/request/UpdateMenuHiddenRequestDto.java
+++ src/main/java/com/example/Spot/menu/presentation/dto/request/UpdateMenuHiddenRequestDto.java
@@ -1,7 +1,5 @@
 package com.example.Spot.menu.presentation.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
--- src/main/java/com/example/Spot/menu/presentation/dto/response/CreateMenuOptionResponseDto.java
+++ src/main/java/com/example/Spot/menu/presentation/dto/response/CreateMenuOptionResponseDto.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import com.example.Spot.menu.domain.entity.MenuOptionEntity;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
