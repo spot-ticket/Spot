5f13efcb179a18223ba1122e8b4b2e4ddfb503b9
yeojun <yeojun@yeojunui-MacBookPro.local>
2026-01-23 17:17:04 +0900

    feat(#215):payment-KakfaConfig생성

---
+++ spot-payment/src/main/java/com/example/Spot/global/infrastructure/config/KafkaConfig.java
@@ -0,0 +1,40 @@
+package com.example.Spot.global.infrastructure.config;
+
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+public class KafkaConfig {
+    
+    @Value("${spring.kafka.bootstrap-serverse")
+    private String bootstrapServers;
+    
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-group");
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+    
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
---
+++ spot-payment/src/main/java/com/example/Spot/payments/event/subscribe/OrderCreatedEvent.java
@@ -0,0 +1,21 @@
+package com.example.Spot.payments.event.subscribe;
+
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class OrderCreatedEvent {
+    private UUID orderId;
+    private Integer userId;
+    private Long amount;
+    private String title;
+    private String content;
+    private String paymentMethod;
+}
