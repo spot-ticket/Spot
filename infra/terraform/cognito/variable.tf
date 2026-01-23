65aaa6727f771099340a274d3f6c27281df1322d
dbswjd7 <yj2171063@ewhain.net>
2026-01-23 17:06:38 +0900

    feat(#227): cognito 토큰 발급 구현

---
+++ infra/terraform/cognito/variable.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  type        = string
+  description = "ap-northeast-2"
+  default     = "ap-northeast-2"
+}
+
+variable "user_service_url" {
+  type        = string
+  default = "http://user-service.internal:8080"
+}
