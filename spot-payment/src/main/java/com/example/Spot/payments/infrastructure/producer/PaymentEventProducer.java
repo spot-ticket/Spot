13b88fd991d19e448e066b9582aa5f9e4b2d2bcb
yeojun <yeojun@yeojunui-MacBookPro.local>
2026-01-25 02:37:18 +0900

    feat(#215):kafka 토픽을 관리하는 yml파일 생성

--- spot-payment/src/main/java/com/example/Spot/payments/infrastructure/producer/PaymentEventProducer.java
+++ spot-payment/src/main/java/com/example/Spot/payments/infrastructure/producer/PaymentEventProducer.java
@@ -1,4 +1,21 @@
 package com.example.Spot.payments.infrastructure.producer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class PaymentEventProducer {
+    
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+    private final String TOPIC = "auth-events-topic";
+    
+    public void sendAuthRequiredEvent
+    
+    
 }
