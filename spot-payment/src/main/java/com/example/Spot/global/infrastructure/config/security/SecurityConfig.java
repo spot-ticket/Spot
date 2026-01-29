9639b36e4cef386d559ce1c1a1749bcfb73d40bd
dbswjd7 <yj2171063@ewhain.net>
2026-01-29 13:51:48 +0900

    feat(#261): import 수정

--- spot-payment/src/main/java/com/example/Spot/global/infrastructure/config/security/SecurityConfig.java
+++ spot-payment/src/main/java/com/example/Spot/global/infrastructure/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.Spot.global.infrastructure.config.security;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
