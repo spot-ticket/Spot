51ff7c8a442d0e7361fecf0396ce536b2a2d370e
yeojun <yeojun@yeojunui-MacBookPro.local>
2026-02-05 00:14:28 +0900

    feat(#285):kakfa-connector 생성

--- spot-order/src/main/java/com/example/Spot/order/application/service/OrderOutboxScheduler.java
+++ spot-order/src/main/java/com/example/Spot/order/application/service/OrderOutboxScheduler.java
@@ -1,38 +1,38 @@
-package com.example.Spot.order.application.service;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.springframework.data.domain.PageRequest;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import com.example.Spot.order.domain.entity.OrderOutboxEntity;
-import com.example.Spot.order.domain.repository.OrderOutboxRepository;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class OrderOutboxScheduler {
-
-    private final OrderOutboxRepository outboxRepository;
-    private final OrderOutboxService orderOutboxService;
-
-    @Scheduled(fixedDelay = 1000)
-    public void processOutbox() {
-        // INIT 상태인 데이터를 오래된 순으로 10개만 가져옴
-        List<OrderOutboxEntity> outboxes = outboxRepository.findTop10ReadyTopublish(
-                LocalDateTime.now(), PageRequest.of(0, 10));
-
-        if (outboxes.isEmpty()) {
-            return;
-        }
-
-        for (OrderOutboxEntity outbox : outboxes) {
-            orderOutboxService.publishIndividualEvent(outbox);
-        }
-    }
-}
+//package com.example.Spot.order.application.service;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import com.example.Spot.order.domain.entity.OrderOutboxEntity;
+//import com.example.Spot.order.domain.repository.OrderOutboxRepository;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class OrderOutboxScheduler {
+//
+//    private final OrderOutboxRepository outboxRepository;
+//    private final OrderOutboxService orderOutboxService;
+//
+//    @Scheduled(fixedDelay = 1000)
+//    public void processOutbox() {
+//        // INIT 상태인 데이터를 오래된 순으로 10개만 가져옴
+//        List<OrderOutboxEntity> outboxes = outboxRepository.findTop10ReadyTopublish(
+//                LocalDateTime.now(), PageRequest.of(0, 10));
+//
+//        if (outboxes.isEmpty()) {
+//            return;
+//        }
+//
+//        for (OrderOutboxEntity outbox : outboxes) {
+//            orderOutboxService.publishIndividualEvent(outbox);
+//        }
+//    }
+//}
