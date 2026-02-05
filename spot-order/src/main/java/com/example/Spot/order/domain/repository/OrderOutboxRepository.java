f17ae676ac7746446d18425887044e7cfa862d25
yeojun <yeojun@yeojunui-MacBookPro.local>
2026-02-05 17:58:55 +0900

    feat(#285):pr-check

--- spot-order/src/main/java/com/example/Spot/order/domain/repository/OrderOutboxRepository.java
+++ spot-order/src/main/java/com/example/Spot/order/domain/repository/OrderOutboxRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.Spot.order.domain.entity.OrderOutboxEntity;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderOutboxRepository extends JpaRepository<OrderOutboxEntity, UUID> {
     
--- spot-payment/src/main/java/com/example/Spot/payments/domain/repository/PaymentOutboxRepository.java
+++ spot-payment/src/main/java/com/example/Spot/payments/domain/repository/PaymentOutboxRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.Spot.payments.domain.entity.PaymentOutboxEntity;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface PaymentOutboxRepository extends JpaRepository<PaymentOutboxEntity, UUID> {
 
