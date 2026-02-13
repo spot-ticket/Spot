4e3d9f6359397c36f13f36ddf5e82ecee228508d
yeojun <yeojun@yeojunui-MacBookPro.local>
2026-02-13 14:19:32 +0900

    feat(#336):local환경 docker-compose를 활용한 카프카 분산 클러스터 구축

--- connectors/register-connectors.sh
+++ connectors/register-connectors.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
 
-TARGET_URL=${CONNECT_URL:-"http://connect:8083"}
+TARGET_URL=${CONNECT_URL:-"http://connect1:8083"}
 
-echo "Waiting for Kafka Connect..."
-while [ $(curl -s -o /dev/null -w "%{http_code}" $TARGET_URL) -ne 200 ]; do
+echo "Waiting for Kafka Connect at $TARGET_URL..."
+
+until [ $(curl -s -o /dev/null -w "%{http_code}" "$TARGET_URL") -eq 200 ]; do
+  echo "Connect is not ready yet... (waiting 3s)"
   sleep 3
 done
 
-echo "Registering connectors from /configs..."
+echo " Kafka Connect is UP. Starting registration..."
+
 for file in /configs/*.json; do
-  filename=$(basename "$file")
-  echo "Processing $filename..."
+  connector_name=$(jq -r '.name' "$file")
+  config_body=$(jq -c '.config' "$file")
 
-  sed -e "s|\${env:DB_HOST}|$DB_HOST|g" \
-      -e "s|\${env:SPRING_DATASOURCE_USERNAME}|$SPRING_DATASOURCE_USERNAME|g" \
-      -e "s|\${env:SPRING_DATASOURCE_PASSWORD}|$SPRING_DATASOURCE_PASSWORD|g" \
-      -e "s|\${env:DB_NAME}|$DB_NAME|g" \
-      "$file" > "/tmp/$filename"
+  echo "Processing Connector: $connector_name"
+  response=$(curl -s -o /dev/null -w "%{http_code}" -X PUT \
+       -H "Content-Type: application/json" \
+       -d "$config_body" \
+       "$TARGET_URL/connectors/$connector_name/config")
 
-  response=$(curl -s -X POST -H "Content-Type: application/json" \
-            -d @"/tmp/$filename" \
-            $TARGET_URL/connectors)
-  echo "Response for $filename: $response"
+  if [ "$response" -eq 200 ] || [ "$response" -eq 201 ]; then
+    echo "Successfully registered/updated: $connector_name"
+  else
+    echo "Failed to register $connector_name (HTTP Status: $response)"
+  fi
 done
 
-echo "ALL connectors Created"
+echo "ALL connectors have been processed!"
